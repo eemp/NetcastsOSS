2b98b30eceabe3e0a9e8e022fb6fa7bf4326cded
Mihir Patel <ee.mihir.patel@gmail.com>
2018-11-11 00:54:56 -0500

    stab at a home page

--- lib/home.dart
+++ lib/home.dart
@@ -7,8 +7,263 @@ class Home extends StatelessWidget {
       appBar: AppBar(
         title: Text('Home'),
       ),
-      body: Center(
-        child: Text('Homepage placeholder'),
+      body: ListView(
+        children: [
+          Container(
+            child: Card(
+              child: Column(
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Text('Your Podcasts', style: Theme.of(context).textTheme.subhead),
+                        Text('MORE', style: TextStyle(color: Theme.of(context).accentColor, fontWeight: FontWeight.bold)),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                  ),
+                  Container(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      physics: PageScrollPhysics(),
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/e8/49/ce/1f/e849ce1f-f43e-4e73-968e-0eca212d2ed6/5549071b8e91edc4dd373526df7cd64d6289c12808e632e2afece7e0258979c0cd5e5848a8e86eebd1e6ebeea8681ac37a23e87c01ade7584ed0bb4480997f06.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Good Place: The Podcast', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/ea/bf/ce/0f/eabfce0f-4d38-4600-b5b9-3e0043b94987/e76f68f407f76ddc2b497f144e037abfe10216f49cae2716c5e174f705b1c05d83f0ff7082da9d6180d42d5a999d7979a140becf59cdc0b91aeaa0e0906793e5.jpeg'),
+                              ),
+                              Container(
+                                child: Text('Star Talk'),
+                                padding: EdgeInsets.only(top: 8.0),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/bd/0b/58/94/bd0b5894-05c5-42c0-910f-8f72e00006b1/e8439cdf81cbe454191c0d23ebba7e1a35e90d7cac14e411bbbd6ebf86ac3bd9d9e79f8cfcf8d07aa4b490658010044771ff51763fee14b30220509cc3a22946.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Daily Show with Trevor Noah: Ears Only Edition', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/e8/49/ce/1f/e849ce1f-f43e-4e73-968e-0eca212d2ed6/5549071b8e91edc4dd373526df7cd64d6289c12808e632e2afece7e0258979c0cd5e5848a8e86eebd1e6ebeea8681ac37a23e87c01ade7584ed0bb4480997f06.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Good Place: The Podcast', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                      ],
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    ),
+                    height: 200.0,
+                  ),
+                ],
+              ),
+            ),
+            height: 246.0,
+            margin: EdgeInsets.all(16.0),
+          ),
+          Container(
+            child: Card(
+              child: Column(
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Text('Recommendations for You', style: Theme.of(context).textTheme.subhead),
+                        Text('MORE', style: TextStyle(color: Theme.of(context).accentColor, fontWeight: FontWeight.bold)),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                  ),
+                  Container(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      physics: PageScrollPhysics(),
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/ea/bf/ce/0f/eabfce0f-4d38-4600-b5b9-3e0043b94987/e76f68f407f76ddc2b497f144e037abfe10216f49cae2716c5e174f705b1c05d83f0ff7082da9d6180d42d5a999d7979a140becf59cdc0b91aeaa0e0906793e5.jpeg'),
+                              ),
+                              Container(
+                                child: Text('Star Talk'),
+                                padding: EdgeInsets.only(top: 8.0),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/bd/0b/58/94/bd0b5894-05c5-42c0-910f-8f72e00006b1/e8439cdf81cbe454191c0d23ebba7e1a35e90d7cac14e411bbbd6ebf86ac3bd9d9e79f8cfcf8d07aa4b490658010044771ff51763fee14b30220509cc3a22946.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Daily Show with Trevor Noah: Ears Only Edition', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/e8/49/ce/1f/e849ce1f-f43e-4e73-968e-0eca212d2ed6/5549071b8e91edc4dd373526df7cd64d6289c12808e632e2afece7e0258979c0cd5e5848a8e86eebd1e6ebeea8681ac37a23e87c01ade7584ed0bb4480997f06.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Good Place: The Podcast', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                      ],
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    ),
+                    height: 200.0,
+                  ),
+                ],
+              ),
+            ),
+            height: 246.0,
+            margin: EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
+          ),
+          Container(
+            child: Card(
+              child: Column(
+                children: [
+                  Container(
+                    child: Row(
+                      children: [
+                        Text('Trending', style: Theme.of(context).textTheme.subhead),
+                        Text('MORE', style: TextStyle(color: Theme.of(context).accentColor, fontWeight: FontWeight.bold)),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    ),
+                    padding: EdgeInsets.only(left: 16.0, right: 16.0, top: 16.0),
+                  ),
+                  Container(
+                    child: ListView(
+                      scrollDirection: Axis.horizontal,
+                      physics: PageScrollPhysics(),
+                      children: [
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/ea/bf/ce/0f/eabfce0f-4d38-4600-b5b9-3e0043b94987/e76f68f407f76ddc2b497f144e037abfe10216f49cae2716c5e174f705b1c05d83f0ff7082da9d6180d42d5a999d7979a140becf59cdc0b91aeaa0e0906793e5.jpeg'),
+                              ),
+                              Container(
+                                child: Text('Star Talk'),
+                                padding: EdgeInsets.only(top: 8.0),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/bd/0b/58/94/bd0b5894-05c5-42c0-910f-8f72e00006b1/e8439cdf81cbe454191c0d23ebba7e1a35e90d7cac14e411bbbd6ebf86ac3bd9d9e79f8cfcf8d07aa4b490658010044771ff51763fee14b30220509cc3a22946.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Daily Show with Trevor Noah: Ears Only Edition', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                        Container(
+                          child: Column(
+                            children: [
+                              Container(
+                                child: Image.network('https://content.production.cdn.art19.com/images/e8/49/ce/1f/e849ce1f-f43e-4e73-968e-0eca212d2ed6/5549071b8e91edc4dd373526df7cd64d6289c12808e632e2afece7e0258979c0cd5e5848a8e86eebd1e6ebeea8681ac37a23e87c01ade7584ed0bb4480997f06.jpeg'),
+                              ),
+                              Flexible(
+                                child: Container(
+                                  child: Text('The Good Place: The Podcast', maxLines: 2, overflow: TextOverflow.clip),
+                                  padding: EdgeInsets.only(top: 8.0),
+                                ),
+                              ),
+                            ],
+                          ),
+                          padding: EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+                          width: 130.0,
+                        ),
+                      ],
+                      padding: EdgeInsets.symmetric(horizontal: 8.0),
+                    ),
+                    height: 200.0,
+                  ),
+                ],
+              ),
+            ),
+            height: 246.0,
+            margin: EdgeInsets.only(bottom: 16.0, left: 16.0, right: 16.0),
+          ),
+        ],
       ),
     );
   }

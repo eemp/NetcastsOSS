8ba57a3c28d6cf37a6d037f6bf1097b702cb074c
Mihir Patel <ee.mihir.patel@gmail.com>
2018-11-24 01:28:03 -0500

    remove unnecessary second home

--- lib/home.dart
+++ lib/home.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
-import 'package:hear2learn/common/horizontal_list_view_card.dart';
-import 'package:hear2learn/podcast/index.dart';
 import 'package:swagger/api.dart';
+
+import 'package:hear2learn/common/horizontal_list_view_card.dart';
+import 'package:hear2learn/episode/index.dart';
+import 'package:hear2learn/podcast/index.dart';
+import 'package:hear2learn/podcast_search/index.dart';
 
 const MAX_SHOWCASE_LIST_SIZE = 20;
 
@@ -40,6 +43,64 @@ class Home extends StatelessWidget {
           ),
           buildToplist(toplistFuture),
         ],
+      ),
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the Drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            Container(
+              child: DrawerHeader(
+                child: Text('Debug Menu'),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).primaryColor,
+                ),
+              ),
+            ),
+            ListTile(
+              title: Text('Homepage'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Home()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Podcast Page'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PodcastPage(
+                    url: 'http://feeds.feedburner.com/thetimferrissshow'
+                    //url: 'http://feeds.feedburner.com/StartupsForTheRestOfUs'
+                  )),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Episode Page'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => Episode()),
+                );
+              },
+            ),
+            ListTile(
+              title: Text('Podcast Search Page'),
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => PodcastSearch()),
+                );
+              },
+            ),
+          ],
+        ),
       ),
     );
   }

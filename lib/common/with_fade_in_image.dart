c232e692065cdde2fe8da1525f4aecc0bf1f77c8
Mihir Patel <ee.mihir.patel@gmail.com>
2018-12-31 20:49:14 -0500

    setup 8 randomly chosen placeholder images

--- lib/common/with_fade_in_image.dart
+++ lib/common/with_fade_in_image.dart
@@ -1,7 +1,21 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class WithFadeInImage extends StatelessWidget {
+  final List<String> availablePlaceholders = [
+   'images/01--default-fadein-image.jpg',
+   'images/02--default-fadein-image.jpg',
+   'images/03--default-fadein-image.jpg',
+   'images/04--default-fadein-image.jpg',
+   'images/05--default-fadein-image.jpg',
+   'images/06--default-fadein-image.jpg',
+   'images/07--default-fadein-image.jpg',
+   'images/08--default-fadein-image.jpg',
+  ];
+  final Random rng = new Random();
+
   String location;
 
   WithFadeInImage({
@@ -11,11 +25,13 @@ class WithFadeInImage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    int randIdx = rng.nextInt(availablePlaceholders.length);
+    String randomPlaceholder = availablePlaceholders[randIdx];
     return FadeInImage.assetNetwork(
       fadeInDuration: Duration(milliseconds: 300),
       fit: BoxFit.fill,
       image: location,
-      placeholder: 'images/default-fadein-image.jpg',
+      placeholder: randomPlaceholder,
       //placeholder: kTransparentImage,
     );
   }

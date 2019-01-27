3c7635f95bf7a9ab667a5c6dc7aee635f3fee2cb
Mihir Patel <ee.mihir.patel@gmail.com>
2019-01-27 15:45:43 -0500

    Revert "simplify podcast page builds"
    This reverts commit 8c958e4ebb0d930abf76d25a8f50f385887a611c.

--- lib/widgets/podcast/index.dart
+++ lib/widgets/podcast/index.dart
@@ -6,6 +6,8 @@ import 'package:hear2learn/widgets/common/bottom_app_bar_player.dart';
 import 'package:hear2learn/widgets/common/toggling_widget_pair.dart';
 import 'package:hear2learn/helpers/episode.dart' as episode_helpers;
 import 'package:hear2learn/helpers/podcast.dart';
+import 'package:hear2learn/models/episode.dart';
+import 'package:hear2learn/models/episode_download.dart';
 import 'package:hear2learn/models/podcast.dart';
 import 'package:hear2learn/models/podcast_subscription.dart';
 import 'package:hear2learn/redux/actions.dart';
@@ -13,7 +15,7 @@ import 'package:hear2learn/widgets/podcast/episodes.dart';
 import 'package:hear2learn/widgets/podcast/home.dart';
 import 'package:hear2learn/services/feeds/podcast.dart';
 
-class PodcastPage extends StatelessWidget {
+class PodcastPage extends StatefulWidget {
   final Widget image;
   final Podcast podcast;
 
@@ -24,11 +26,24 @@ class PodcastPage extends StatelessWidget {
   }) : super(key: key);
 
   @override
+  PodcastPageState createState() => PodcastPageState();
+}
+
+class PodcastPageState extends State<PodcastPage> {
+  final App app = App();
+
+  Widget get image => widget.image;
+  Podcast get podcast => widget.podcast;
+
+  @override
   Widget build(BuildContext context) {
-    final App app = App();
     final PodcastSubscriptionBean subscriptionModel = app.models['podcast_subscription'];
 
-    final Future<Podcast> podcastFuture = getPodcastFromFeed(podcast: podcast);
+    final Future<Podcast> podcastFuture = getPodcastFromFeed(podcast: podcast).then(
+      (Podcast podcast) => Future.wait(podcast.episodes.map((Episode episode) => episode.getDownload())).then(
+        (List<EpisodeDownload> downloads) => Future<Podcast>.value(podcast)
+      )
+    );
     final Future<PodcastSubscription> podcastSubscriptionFuture = subscriptionModel.findOneWhere(subscriptionModel.podcastUrl.eq(podcast.feed))
       .then((PodcastSubscription response) => response ?? PodcastSubscription(isSubscribed: false));
 
@@ -73,13 +88,11 @@ class PodcastPage extends StatelessWidget {
   }
 
   Future<void> onUnsubscribe() async {
-    final App app = App();
     final PodcastSubscriptionBean subscriptionModel = app.models['podcast_subscription'];
     await subscriptionModel.removeWhere(subscriptionModel.podcastUrl.eq(podcast.feed));
   }
 
   Widget buildSubscriptionButton(Future<PodcastSubscription> podcastSubscriptionFuture) {
-    final App app = App();
     return FutureBuilder<PodcastSubscription>(
       future: podcastSubscriptionFuture,
       builder: (BuildContext context, AsyncSnapshot<PodcastSubscription> snapshot) {

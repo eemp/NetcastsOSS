097a20b3b74d8412e753feb4b22d772c148d2820
Mihir Patel <ee.mihir.patel@gmail.com>
2019-01-27 00:09:45 -0500

    Update prod entry with bonsai url

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -6,7 +6,8 @@ import 'package:hear2learn/run_app.dart';
 Future<void> main() async {
   final App app = App();
   await app.init(
-    elasticHost: 'hear2learn.azurewebsites.net',
+    //elasticHost: 'hear2learn.azurewebsites.net',
+    elasticHost: 'qjrddhgpbq:41vtfspb70@podcasts-9232921312.us-east-1.bonsaisearch.net',
   );
 
   await start();

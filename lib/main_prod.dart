de17bab54e911aac2ee4d5632e634264fb9eae88
Mihir Patel <ee.mihir.patel@gmail.com>
2019-05-18 22:30:01 -0400

    Switch to using serverless obscuring access to datastore

--- lib/main_prod.dart
+++ lib/main_prod.dart
@@ -10,8 +10,8 @@ import 'package:hear2learn/run_app.dart';
 Future<void> main() async {
   final App app = App();
   await app.init(
-    //elasticHost: 'hear2learn.azurewebsites.net',
-    elasticHost: 'qjrddhgpbq:41vtfspb70@podcasts-9232921312.us-east-1.bonsaisearch.net',
+    elasticHost: 'hear2learn.azurewebsites.net',
+    //elasticHost: 'qjrddhgpbq:41vtfspb70@podcasts-9232921312.us-east-1.bonsaisearch.net',
   );
 
   final FirebaseAnalytics analytics = FirebaseAnalytics();
